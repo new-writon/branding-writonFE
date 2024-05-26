f5be4188a9c63171b9bc7c77843f2972bd8bc87f
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-05-27 00:39:25 +0900

    feat : MainTitle component

---
+++ components/home/MainTitle/MainTitle.tsx
@@ -0,0 +1,16 @@
+import styles from './MainTitle.module.css'
+import { StartButton } from '@/components/atom/start-button'
+
+export default function MainTitle() {
+  return (
+    <div className={styles.container}>
+      <span className={styles.main_text}>글과 회고로 함께 성장하는 공간</span>
+      <span className={styles.sub_text}>
+        라이톤에서 팀 회고를 작성하고 관리하세요.
+        <br />
+        그리고 팀에 맞춤화 된 프라이빗한 공간을 누리세요.
+      </span>
+      <StartButton backgroundColor="purple" />
+    </div>
+  )
+}
