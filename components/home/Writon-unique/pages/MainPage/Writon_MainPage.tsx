12e38443f2eadd3fe2a99fb358f32a8fc1decc40
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-05-27 00:40:36 +0900

    feat : writon mainPage component

---
+++ components/home/Writon-unique/pages/MainPage/Writon_MainPage.tsx
@@ -0,0 +1,10 @@
+import ProgressBox from '../../components/MainPage/ProgressBox/ProgressBox'
+import styles from './Writon_MainPage.module.css'
+
+export default function Writon_MainPage() {
+  return (
+    <div className={styles.container}>
+      <ProgressBox />
+    </div>
+  )
+}
