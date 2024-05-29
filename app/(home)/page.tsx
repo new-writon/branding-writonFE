4b59270152a9822e58d2255d6da4428f18ad2591
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-05-30 01:20:28 +0900

    feat : home second_section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,20 @@
-import Writon_MainPage from '@/components/home/Writon-unique/pages/MainPage/Writon_MainPage'
-import styles from './home.module.css'
-import MainTitle from '@/components/home/MainTitle/MainTitle'
+import Writon_MainPage from '@/components/home/Writon-unique/pages/MainPage/Writon_MainPage';
+import styles from './home.module.css';
+import MainTitle from '@/components/home/MainTitle/MainTitle';
+import WritonFunctionIntroduce from '@/components/home/WritonFunctionIntroduce/WritonFunctionIntroduce';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <MainTitle />
-      <Writon_MainPage />
+      <section className={styles.first_section}>
+        <MainTitle />
+        <Writon_MainPage />
+      </section>
+      <section className={styles.second_section}>
+        <section className={styles.second_white_section}>
+          <WritonFunctionIntroduce />
+        </section>
+      </section>
     </div>
-  )
+  );
 }
