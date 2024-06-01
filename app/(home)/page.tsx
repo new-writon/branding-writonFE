964a779002df682a3cae6ccd0ab469a8fb943f5d
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-06-01 15:52:20 +0900

    fix : home page add element

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,9 @@ import Writon_MainPage from '@/components/home/Writon-unique/pages/MainPage/Writ
 import styles from './home.module.css';
 import MainTitle from '@/components/home/MainTitle/MainTitle';
 import WritonFunctionIntroduce from '@/components/home/WritonFunctionIntroduce/WritonFunctionIntroduce';
+import WritonInvite from '@/components/home/WritonInvite/WritonInvite';
+import WritonUse from '@/components/home/WritonUse/WritonUse';
+import WritonRecommend from '@/components/home/WritonRecommend/WritonRecommend';
 
 export default function Home() {
   return (
@@ -14,6 +17,17 @@ export default function Home() {
         <section className={styles.second_white_section}>
           <WritonFunctionIntroduce />
         </section>
+      </section>
+      <section className={styles.third_section}>
+        <section className={styles.third_white_section}>
+          <WritonInvite />
+        </section>
+      </section>
+      <section className={styles.forth_section}>
+        <WritonUse />
+      </section>
+      <section className={styles.fifth_section}>
+        <WritonRecommend />
       </section>
     </div>
   );
