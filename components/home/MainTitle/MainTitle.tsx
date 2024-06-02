9992e69f477227789dd06869c14ed242ba077fe8
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-06-02 16:39:50 +0900

    design : first-section animation

--- components/home/MainTitle/MainTitle.tsx
+++ components/home/MainTitle/MainTitle.tsx
@@ -1,9 +1,16 @@
+'use client';
+
 import styles from './MainTitle.module.css';
 import { StartButton } from '@/components/atom/start-button';
-
+import { motion } from 'framer-motion';
 export default function MainTitle() {
   return (
-    <div className={styles.container}>
+    <motion.div
+      className={styles.container}
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.7, ease: 'easeInOut' }}
+    >
       <span className={styles.main_text}>글과 회고로 함께 성장하는 공간</span>
       <span className={styles.sub_text}>
         라이톤에서 팀 회고를 작성하고 관리하세요.
@@ -11,6 +18,6 @@ export default function MainTitle() {
         그리고 팀에 맞춤화 된 프라이빗한 공간을 누리세요.
       </span>
       <StartButton backgroundColor="purple" />
-    </div>
+    </motion.div>
   );
 }
