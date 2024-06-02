41e134856fa2bc7dc1bdf28f8ca9b9b48d481f25
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-06-02 16:54:05 +0900

    feat : writonInvite-section  scroll animation

--- components/home/WritonUse/WritonUse.tsx
+++ components/home/WritonUse/WritonUse.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './WritonUse.module.css';
 
 import image_desktop from '@/public/image/mainPage/use-type/image-desktop-use.png';
@@ -20,11 +20,39 @@ const imageMobileStyle = {
 
 export default function WritonUse() {
   const [active, setActive] = useState<boolean>(false);
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setActive(true);
+        } else if (entry.boundingClientRect.top > 0) {
+          setActive(false); // 요소가 화면 위로 올라가면 active 해제
+        }
+      },
+      {
+        threshold: 0.3, // 요소가 50% 이상 보이면 콜백 함수 실행
+      },
+    );
+
+    if (elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+
+    return () => {
+      if (elementRef.current) {
+        observer.unobserve(elementRef.current);
+      }
+    };
+  }, []);
 
   return (
-    <div className={`${styles.container} ${active && styles.active}`}>
+    <div
+      ref={elementRef}
+      className={`${styles.container} ${active && styles.active}`}
+    >
       <p className={styles.title}>PC와 모바일에서 모두 사용할 수 있어요</p>
-      <button onClick={() => setActive(!active)}>이벤트</button>
       <Image
         src={image_desktop}
         alt="desktop"
