e250c54dc3ffbee076dedc46198422c9fbf0b67c
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-06-02 16:39:05 +0900

    fix : start-button component (add arrow-image)

--- components/atom/start-button.tsx
+++ components/atom/start-button.tsx
@@ -1,24 +1,41 @@
-'use client'
+'use client';
 
-import styles from '@/styles/atom/start-button.module.css'
+import start_arrow from '@/public/image/icon/start_arrow.svg';
+import start_arrow_purple from '@/public/image/icon/start_arrow_purple.svg';
+import styles from '@/styles/atom/start-button.module.css';
+import Image from 'next/image';
+import { useState } from 'react';
 export function StartButton({ backgroundColor }: { backgroundColor: string }) {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+
   return (
     <button
-      onClick={() => window.open('https://writon.co.kr', '_blank')}
-      className={`${styles.button} ${backgroundColor === 'purple' ? styles.purple : styles.white}`}
+      onClick={() => window.open('https://tally.so/r/3qbyRk', '_blank')}
+      onMouseOver={() => setIsHovered(true)}
+      onMouseOut={() => setIsHovered(false)}
+      className={`${styles.button} ${backgroundColor === 'purple' ? styles.purple : styles.white} ${isHovered && styles.hover}`}
     >
-      시작하기
+      <span>시작하기</span>
+      <Image
+        src={backgroundColor === 'purple' ? start_arrow : start_arrow_purple}
+        alt="->"
+      />
     </button>
-  )
+  );
 }
 
 export function HeaderStartButton() {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+
   return (
     <button
-      onClick={() => window.open('https://writon.co.kr', '_blank')}
-      className={styles.header_button}
+      onClick={() => window.open('https://tally.so/r/3qbyRk', '_blank')}
+      onMouseOver={() => setIsHovered(true)}
+      onMouseOut={() => setIsHovered(false)}
+      className={`${styles.header_button} ${isHovered && styles.hover}`}
     >
-      시작하기
+      <span>시작하기</span>
+      <Image src={start_arrow} alt="->" />
     </button>
-  )
+  );
 }
