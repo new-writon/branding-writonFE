9f8549a154d6bfe285f02a6b160518387a5c5c2a
hoyyChoi <hoyeon8165@gmail.com>
2025-06-26 16:53:42 +0900

    hotfix: 시작하기 버튼 url 변경

--- components/atom/start-button.tsx
+++ components/atom/start-button.tsx
@@ -10,7 +10,10 @@ export function StartButton({ backgroundColor }: { backgroundColor: string }) {
 
   return (
     <button
-      onClick={() => window.open('https://tally.so/r/3qbyRk', '_blank')}
+      // onClick={() => window.open('https://tally.so/r/3qbyRk', '_blank')}
+      onClick={() => {
+        window.open('https://www.writon.co.kr/login', '_blank');
+      }}
       onMouseOver={() => setIsHovered(true)}
       onMouseOut={() => setIsHovered(false)}
       className={`${styles.button} ${backgroundColor === 'purple' ? styles.purple : styles.white} ${isHovered && styles.hover}`}
@@ -29,7 +32,10 @@ export function HeaderStartButton() {
 
   return (
     <button
-      onClick={() => window.open('https://tally.so/r/3qbyRk', '_blank')}
+      // onClick={() => window.open('https://tally.so/r/3qbyRk', '_blank')}
+      onClick={() => {
+        window.open('https://www.writon.co.kr/login', '_blank');
+      }}
       onMouseOver={() => setIsHovered(true)}
       onMouseOut={() => setIsHovered(false)}
       className={`${styles.header_button} ${isHovered && styles.hover}`}
